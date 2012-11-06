a971bc27e51c27454ba8655047cf6401fc28096d
beville@gmail.com <beville@gmail.com@6c5673fe-1810-88d6-992b-cd32ca31540c>
2012-11-06 20:56:30 +0000

    Added apache license headers
    git-svn-id: http://comictagger.googlecode.com/svn/trunk@13 6c5673fe-1810-88d6-992b-cd32ca31540c

--- tagger.py
+++ tagger.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python
 
 """
-A python script to tag CBZ files
+A python script to tag comic archives
+"""
+
+"""
+Copyright 2012  Anthony Beville
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 import sys
@@ -28,6 +44,8 @@ import utils
 #-----------------------------
 def cliProcedure( opts ):
 
+	pass
+	"""
 	comicVine = ComicVineTalker()
 
 	cv_search_results = comicVine.searchForSeries( opts.series_name )
@@ -51,7 +69,7 @@ def cliProcedure( opts ):
 	#debugging
 	ComicBookInfo().writeToExternalFile( "test.json" )
 	ComicBookInfo().writeToExternalFile( "test.xml" )
-
+	"""
 #-----------------------------
 
 def main():
