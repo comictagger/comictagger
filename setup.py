22ef0250ca157ae8c0346021d4b07364d97b7529
beville@gmail.com <beville@gmail.com@6c5673fe-1810-88d6-992b-cd32ca31540c>
2013-02-07 22:38:27 +0000

    python packaging tweaks
    git-svn-id: http://comictagger.googlecode.com/svn/trunk@463 6c5673fe-1810-88d6-992b-cd32ca31540c

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@ from distutils.core import setup
 import comictaggerlib.ctversion
 
 setup(name = "comictagger",
-      version =  comictaggerlib.ctversion.version,
-      description = "A cross-platorm GUI/CLI app for writing metadata to comic archives",
-      author = "Anthony Beville",
-      author_email = "comictagger@gmail.com",
-      url = "http://code.google.com/p/comictagger/",
-      packages =  [ "comictaggerlib", "comictaggerlib/UnRAR2" ] ,
-      package_data = {
-        'comictaggerlib': ['ui/*.ui', 'graphics/*'] ,
-        'comictaggerlib/UnRAR2': ['UnRARDLL/*.*', 'UnRARDLL/x64/*.*'] ,
-      },
-      scripts = ["comictagger.py"],
-      license = "Apache License 2.0",
-      long_description = """
+    version =  comictaggerlib.ctversion.version,
+    description = "A cross-platorm GUI/CLI app for writing metadata to comic archives",
+    author = "Anthony Beville",
+    author_email = "comictagger@gmail.com",
+    url = "http://code.google.com/p/comictagger/",
+    packages =  [ "comictaggerlib", "comictaggerlib/UnRAR2" ] ,
+    package_data = {
+      'comictaggerlib': ['ui/*.ui', 'graphics/*'] ,
+      'comictaggerlib/UnRAR2': ['UnRARDLL/*.*', 'UnRARDLL/x64/*.*'] ,
+    },
+    scripts = ["comictagger.py"],    
+    license = "Apache License 2.0",
+    
+    long_description = """
 ComicTagger is a multi-platform app for writing metadata to comic archives, written in Python and PyQt.
 
 Features:
@@ -28,5 +29,15 @@ Features:
 * Reads and writes multiple tagging schemes ( ComicBookLover? and ComicRack?, with more planned).
 * Reads and writes RAR, Zip, and folder archives (external tools needed for writing RAR)
 * Command line interface (CLI) on all platforms (including Windows), which supports batch operations, and which can be used in native scripts for complex operations. 
+
+Requires:
+
+* python 2.6 or 2.7
+* python imaging (PIL) >= 1.1.7
+* beautifulsoup > 4.1
+    
+Optional requirement (for GUI):
+
+* pyqt4
 """          
       )    
