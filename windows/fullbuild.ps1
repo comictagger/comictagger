15f6b26326ddf398cba2e20dade6cd3e296cdd66
Davide Romanini <davide.romanini@gmail.com>
2018-04-17 14:43:10 -0700

    #108 feat: fix python location on appveyor (try 1)

--- windows/fullbuild.ps1
+++ windows/fullbuild.ps1
@@ -1,5 +1,5 @@
 # Script to be run inside appveyor for a full build
 $env:PATH="C:\Python36-x64;$env:path"
+C:\Python36-x64\Scripts\pip install -r .\requirements.txt
 choco install -y mingw
-C:\Miniconda-x64\Scripts\pip install -r .\requirements.txt
 mingw32-make dist
