84ccbff70660f40cb0d50d94cd643d11618fa3f8
Davide Romanini <davide.romanini@gmail.com>
2018-04-17 15:25:31 -0700

    #108 feat: use venv (try 3)

--- windows/fullbuild.ps1
+++ windows/fullbuild.ps1
@@ -1,7 +1,7 @@
 # Script to be run inside appveyor for a full build
 $env:PATH="C:\Python36-x64;$env:path"
 python -m venv venv
-.\venv\Scripts\Activate.ps
+.\venv\Scripts\Activate.ps1
 pip install -r .\requirements.txt
 choco install -y mingw
 mingw32-make dist
