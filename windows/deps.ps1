5416bb15c34bfb56a4dec6bddafb653c3be9a187
davide-romanini <davide.romanini@gmail.com>
2019-09-24 23:36:08 +0100

    Appveyor GitHub release (#139)

--- windows/deps.ps1
+++
@@ -1,5 +0,0 @@
-# to be run inside comictagger folder
-# installs conda and pip dependencies
-activate comictagger
-conda install -y pyqt=4
-pip install -r .\requirements.txt
--- windows/env.ps1
+++
@@ -1,3 +0,0 @@
-$env:PATH+=";C:\ProgramData\Miniconda2\Scripts;C:\ProgramData\Miniconda2"
-$env:PATH+=";C:\tools\mingw64\bin"
-Set-Alias make mingw32-make.exe -scope Global
