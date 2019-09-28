1e832ecd9cdd067aeea20e613b8ff9e0815ab80a
Davide Romanini <davide.romanini@gmail.com>
2019-09-28 15:13:04 +0200

    add travis config for all platforms

--- windows/fullbuild.ps1
+++
@@ -1,8 +0,0 @@
-# Script to be run inside appveyor for a full build
-choco install -y mingw zip
-refreshenv
-$env:PATH="C:\Python36-x64;$env:path"
-python -m venv venv
-.\venv\Scripts\Activate.ps1
-pip install -r .\requirements.txt
-mingw32-make dist
