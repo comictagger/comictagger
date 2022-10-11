c1c8f4eb6ed0b0a2d0226ff4fea2434c3a2838e8
Mizaki <jinxybob@hotmail.com>
2022-10-12 00:11:57 +0100

    black

--- tests/issueidentifier_test.py
+++ tests/issueidentifier_test.py
@@ -51,7 +51,7 @@ def test_get_issue_cover_match_score(cbz, settings, comicvine_api):
 
 
 def test_search(cbz, settings, comicvine_api):
-    ii = comictaggerlib.issueidentifier.IssueIdentifier(cbz, settings, comictalker.comictalker.ComicTalker('comicvine'))
+    ii = comictaggerlib.issueidentifier.IssueIdentifier(cbz, settings, comictalker.comictalker.ComicTalker("comicvine"))
     results = ii.search()
     cv_expected = {
         "series": f"{testing.comicvine.cv_volume_result['results']['name']} ({testing.comicvine.cv_volume_result['results']['start_year']})",
