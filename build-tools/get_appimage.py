f94c9ef8574b3b47978c7e8e920426c529531499
Timmy Welch <timmy@narnian.us>
2023-04-24 22:59:50 -0700

    Update appimage step
    Fix platform case
    Remove icu check from appimage step as ComicTagger is not installed
    Add appimagetool to allowed commands
    Fix appimage paths

--- build-tools/get_appimage.py
+++ build-tools/get_appimage.py
@@ -1,26 +1,33 @@
 from __future__ import annotations
 
+import argparse
 import os
 import pathlib
 import stat
 
 import requests
 
+parser = argparse.ArgumentParser()
+parser.add_argument("APPIMAGETOOL", default="build/appimagetool-x86_64.AppImage", type=pathlib.Path, nargs="?")
 
-def urlretrieve(url: str, dest: str) -> None:
+opts = parser.parse_args()
+opts.APPIMAGETOOL = opts.APPIMAGETOOL.absolute()
+
+
+def urlretrieve(url: str, dest: pathlib.Path) -> None:
     resp = requests.get(url)
     if resp.status_code == 200:
-        pathlib.Path(dest).write_bytes(resp.content)
+        dest.parent.mkdir(parents=True, exist_ok=True)
+        dest.write_bytes(resp.content)
 
 
-APPIMAGETOOL = "build/appimagetool-x86_64.AppImage"
-if os.path.exists(APPIMAGETOOL):
+if opts.APPIMAGETOOL.exists():
     raise SystemExit(0)
 
 urlretrieve(
-    "https://github.com/AppImage/AppImageKit/releases/latest/download/appimagetool-x86_64.AppImage", APPIMAGETOOL
+    "https://github.com/AppImage/AppImageKit/releases/latest/download/appimagetool-x86_64.AppImage", opts.APPIMAGETOOL
 )
-os.chmod(APPIMAGETOOL, stat.S_IRWXU)
+os.chmod(opts.APPIMAGETOOL, stat.S_IRWXU)
 
-if not os.path.exists(APPIMAGETOOL):
+if not opts.APPIMAGETOOL.exists():
     raise SystemExit(1)
