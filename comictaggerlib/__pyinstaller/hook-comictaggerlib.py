05a82ddfdb589ef1b26044a355d5f8b3a39437a8
Timmy Welch <timmy@narnian.us>
2024-02-20 20:44:48 -0800

    Fix PyInstaller packaging

--- comictaggerlib/__pyinstaller/hook-comictaggerlib.py
+++ comictaggerlib/__pyinstaller/hook-comictaggerlib.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from PyInstaller.utils.hooks import collect_data_files, collect_entry_point
+from PyInstaller.utils.hooks import collect_data_files, collect_entry_point, collect_submodules
 
 datas, hiddenimports = collect_entry_point("comictagger.talker")
+hiddenimports += collect_submodules("comictaggerlib")
 datas += collect_data_files("comictaggerlib.ui")
 datas += collect_data_files("comictaggerlib.graphics")
